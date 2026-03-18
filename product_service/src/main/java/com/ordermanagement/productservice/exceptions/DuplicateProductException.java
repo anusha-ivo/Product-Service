e8ca848dcc18390a671a30214cbd095b8e046f7f
anusha-ivo <anusha525@gmail.com>
2026-03-18 23:39:00 +0530

    product

--- product_service/src/main/java/com/ordermanagement/productservice/exceptions/DuplicateProductException.java
+++
@@ -1,9 +0,0 @@
-package com.ordermanagement.productservice.exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public class DuplicateProductException extends ProductException {
-    public DuplicateProductException(String message) {
-        super(message, HttpStatus.CONFLICT, "DUPLICATE_PRODUCT");
-    }
-}
